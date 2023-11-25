8aff64a58588b63bcdd8caf2f7dc923e0fcbede3
Gabriel Moskwa <88377577+dotxdd@users.noreply.github.com>
2023-11-25 11:13:27 +0100

    Add files via upload

--- listy.pl
+++ listy.pl
@@ -13,3 +13,14 @@ dlugosc([], 0).
 dlugosc([_ | Reszta], Dlugosc) :-
     dlugosc(Reszta, DlugoscOgonu),
     Dlugosc is DlugoscOgonu + 1.
+polacz([], Lista, Lista).
+
+polacz([Glowa | Ogon], Lista2, [Glowa | Wynik]) :-
+    polacz(Ogon, Lista2, Wynik).
+dodaj(X, Lista, [X|Lista]).
+
+run:-
+    [X|Y] = [1,2,3,4],
+    write('zadanie 1'), nl pisz([X|Y]), nl,
+    write('zadanie 2'), nl, write('dlugosc listy '), dlugosc([X|Y], D),
+    write(D).
